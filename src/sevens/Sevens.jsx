10b7681d78799497bc2e89476dab32bf026f20b7
eieiouka <eieiyukkuri@gmail.com>
2026-07-23 01:16:36 +0900

    J

--- src/sevens/Sevens.jsx
+++ src/sevens/Sevens.jsx
@@ -1612,6 +1612,7 @@ useEffect(() => {
       setRoundResult(null);
       setFinalResultVisible(false);
       setWinnerType(null);
+      setWinnerIndex(null);
 
       roundRecordedRef.current = false;
 
